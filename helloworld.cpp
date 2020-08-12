11aede6d8ef59802366cfaf3adf15f700b9b786b
Rishabh <hersh2704@gmail.com>
2020-08-12 13:53:54 +0530

    Changed the helloworld program updated with comments after each line

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
+#include <iostream>  // Header files used for input output statements used in c++ program 
 
-using namespace std;
+using namespace std; // it basically is used for standard names for objects,variabes and function in c++ to uniquely distinguish them from other members in the library 
 
-int main()
+int main()  //This is the main function which runs the c++ code inside it 
 {
-    cout<<"hello world"<<endl;
+    cout<<"hello world"<<endl;  // cout statement is us to print the output on on the screen
     int a , b = 3;
-    cout<<"enter two nos."<<endl;
-    cin>>a;
+    cout<<"enter two nos."<<endl; // endl is used to go on to next line 
+    cin>>a; // cin statement is used to take input from a user 
     cin>>b;
-    cout<<a<<"\n"<<b<<"\n"<<"sum ="<<a+b;
+    cout<<a<<"\n"<<b<<"\n"<<"sum ="<<a+b; // this style is refered to as csascading in c++ where you print multple things in single line using output "<<" operator
 }
