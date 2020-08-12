32070bedb8857f315ab017e6181d3496cc1965f8
Rishabh <hersh2704@gmail.com>
2020-08-12 13:37:12 +0530

    addded helloword.cpp program

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"hello world"<<endl;
+    int a , b = 3;
+    cout<<"enter two nos."<<endl;
+    cin>>a;
+    cin>>b;
+    cout<<a<<"\n"<<b<<"\n"<<"sum ="<<a+b;
+}
